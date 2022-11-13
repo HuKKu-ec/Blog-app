0a5bb59ac6f5ac1c7ee070894968eb29fb60485c
unknown <hukbathelachola2311@gmail.com>
2022-11-13 16:17:05 +0530

    some changes are made

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 import { BlogsContextProvider } from './Context/BlogsContext';
 import { UserContextProvider } from './Context/UserContext';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <UserContextProvider>
